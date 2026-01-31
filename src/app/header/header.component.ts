5581a8c67b5b04f414ef1c1277d5ef5a84b28419
Eslam Gomaa <engeslam399@gmail.com>
2026-01-31 03:04:21 +0200

    feat: implement a responsive header component with navigation, user profile, and sidebar drawer.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,19 +3,25 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { Popover } from 'primeng/popover';
+import { Drawer } from 'primeng/drawer';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [RouterLink, RouterLinkActive, CommonModule, Popover],
+    imports: [RouterLink, RouterLinkActive, CommonModule, Popover, Drawer],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
 export class HeaderComponent {
     isMenuOpen = signal(false);
+    sidebarVisible: boolean = false;
 
     toggleMenu() {
         this.isMenuOpen.update(v => !v);
+    }
+
+    toggleSidebar() {
+        this.sidebarVisible = !this.sidebarVisible;
     }
 
     closeMenu() {
