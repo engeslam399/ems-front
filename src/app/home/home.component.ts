e1815391401c63e1cff554c6a8c86a250585a898
Eslam Gomaa <engeslam399@gmail.com>
2026-01-31 02:17:55 +0200

    add primng bootstrab

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
 
 @Component({
     selector: 'app-home',
     standalone: true,
+    imports: [Button],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
